8ef9b04cb9ae93123e1b4c701c63bf125e2ee398
zanylbajguttieva <janabaiguttieva@gmail.com>
2024-01-31 22:23:57 +0600

    Added 3 types

---
+++ types.d.ts
@@ -0,0 +1,28 @@
+import {resolveCaa} from "node:dns";
+
+export interface Category{
+    id: string;
+    title: string;
+    description: string;
+}
+
+export type CategoryWithoutId = Omit<Category, 'id'>;
+
+export interface Location {
+    id: string;
+    name: string;
+    description: string;
+}
+
+export type LocationWithoutId = Omit<Location, 'id'>;
+
+export interface Item {
+    id: string;
+    category_id: string;
+    location_id: string;
+    name: string;
+    description: string;
+    image: string;
+}
+
+export type ItemWithoutId = Omit<Item, 'id'>;
