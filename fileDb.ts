3926daa437373d867449f4afb72dbc82380d93fa
zanylbajguttieva <janabaiguttieva@gmail.com>
2024-01-31 23:38:51 +0600

    Added data and dataLocation in one

--- fileDb.ts
+++ fileDb.ts
@@ -1,31 +1,62 @@
 import {promises as fs} from 'fs';
 import crypto from 'crypto';
-import {Category, CategoryWithoutId} from "./types";
+import {Category, CategoryWithoutId, Location, LocationWithoutId} from "./types";
 const fileName = './db.json';
 let data: Category[] = [];
+let dataLocation: Location[] = [];
 
 const fileDb = {
     async init() {
         try {
             const fileContents = await fs.readFile(fileName);
-            data = JSON.parse(fileContents.toString());
+            const parsedData = JSON.parse(fileContents.toString());
+
+            if(parsedData.categories) {
+                data = parsedData.categories;
+            } else {
+                data = [];
+            }
+
+            if(parsedData.locations) {
+                dataLocation = parsedData.locations;
+            } else {
+                dataLocation = [];
+            }
         } catch (e) {
             data = [];
+            dataLocation = [];
         }
     },
     async getItems() {
-        return data;
+        const dataToGet = {
+            categories: data,
+            locations: dataLocation,
+        }
+        return dataToGet;
     },
-    async addItem(item: CategoryWithoutId) {
+    async addItem(file: CategoryWithoutId) {
         const id = crypto.randomUUID();
-        const response = {id, ...item};
+        const response = {id, ...file};
         data.push(response);
         await this.save();
 
         return response;
     },
+    async addLocation(file: LocationWithoutId) {
+        const id = crypto.randomUUID();
+        const responseLocation = {id, ...file};
+
+        dataLocation.push(responseLocation);
+        await this.save();
+
+        return responseLocation;
+    },
     async save() {
-        return fs.writeFile(fileName, JSON.stringify(data));
+        const dataToSave = {
+            categories: data,
+            locations: dataLocation,
+        }
+        return fs.writeFile(fileName, JSON.stringify(dataToSave));
     }
 };
 
